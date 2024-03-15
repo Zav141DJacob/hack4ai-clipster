ece6705cb93012f7ba74f0f92b41f910a4be66bb
Jaqub <jaagup.tomingas@gmail.com>
2024-03-16 01:54:54 +0200

    Added default iPhone fonts; Added design from figma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      regular: 'regular',
+      regularBold: 'regularBold',
+    },
+    colors: {
+      white: "#FFFFFF",
+      darkGray: "#2B2D42",
+      gray: "#8D99AE",
+      lightGray: "#EDF2F4",
+      myRed1: "#EF233C",
+      myRed2: "#D90429",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
