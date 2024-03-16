2e1e1b5012ea8bc9701006b56c7f9fce0a5262cb
Haider Mansoor <haider@bitfactory.io>
2024-03-16 18:15:05 +0200

    changes

--- src/context/QuestionContext.tsx
+++ src/context/QuestionContext.tsx
@@ -3,21 +3,27 @@ import React, {
   Dispatch,
   SetStateAction,
   useContext,
+  useEffect,
   useState,
 } from "react";
 import { Question } from "@/types/Question";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export interface QuestionContext {
   questions: Question[];
   setQuestions: Dispatch<SetStateAction<Question[]>>;
   fetchQuestions: (text: string) => Promise<void>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const defaultState: QuestionContext = {
   questions: [],
   setQuestions: () => {},
   fetchQuestions: async () => {},
+  loading: false,
+  setLoading: () => false,
 };
 
 export const QuestionsContext = createContext<QuestionContext>(defaultState);
@@ -31,20 +37,32 @@ export const useQuestions = () => {
 };
 
 const QuestionsProvider = ({ children }: QuestionProvidedProps) => {
+  const router = useRouter();
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const fetchQuestions = async (text: string) => {
     try {
+      setLoading(true);
       const response = await axios.post("/api/questions", { text });
       setQuestions(response.data.questions);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching questions:", error);
     }
   };
 
+  useEffect(() => {
+    if (questions.length >= 1) {
+      router.push("/Questions");
+    } else {
+      router.push("/");
+    }
+  }, [questions]);
+
   return (
     <QuestionsContext.Provider
-      value={{ questions, setQuestions, fetchQuestions }}
+      value={{ questions, setQuestions, fetchQuestions, loading, setLoading }}
     >
       {children}
     </QuestionsContext.Provider>
